1bfb18e35b7dc20b0a37c427c39c345ba0486bfa
kano <kano@nextconceptdc.com>
2013-06-02 01:52:48 +0900

    railsプロジェクト作成スクリプトのDB作成に、文字コードの設定を追加。

--- cookbooks/ruby/files/default/create_rails_project.sh
+++ cookbooks/ruby/files/default/create_rails_project.sh
@@ -21,7 +21,7 @@ EOS
 bundle install --path vendor/bundle 2> /dev/null > /dev/null
 bundle exec rails new "$1" -d mysql --skip-bundle 2> /dev/null > /dev/null
 DB_NAME=`basename $1`
-mysql -u root -p -e "create database ${DB_NAME}_development;create database ${DB_NAME}_test;create database ${DB_NAME}_production;"
+mysql -u root -p -e "create database ${DB_NAME}_development default character set utf8;create database ${DB_NAME}_test default character set utf8;create database ${DB_NAME}_production default character set utf8;"
 rm -rf ${tmp_dir} 
 
 echo "Succeeded in creating rails project"

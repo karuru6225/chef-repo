854913a2e1d5755b756e9e5825057130e91348d1
kano <kano@nextconceptdc.com>
2013-06-07 16:17:24 +0900

    ,

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -1,13 +1,13 @@
 case platform
 	when "centos"
 		default['php']['packages'] = [
-			"php"
-			"php-mbstring"
+			"php",
+			"php-mbstring",
 			"php-mysql"
 		]
 	when "debian"
 		default['php']['packages'] = [
-			"php5"
+			"php5",
 			"php5-mysql"
 		]
 end

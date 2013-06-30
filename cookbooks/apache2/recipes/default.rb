f0ebf1f3de147b41054f8d19a54da16f568e2dbe
kano <kano@ncdc.co.jp>
2013-06-30 09:55:31 +0900

    apacheの設定を環境別に。メインの設定ファイルを一旦対象外に。

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -21,20 +21,23 @@ service "apache2" do
 	action [:enable]
 end
 
-cookbook_file node['apache']['home'] + '/' + node['apache']['conffile'] do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source 'httpd.conf'
-	action :create
-	notifies :restart, "service[apache2]"
-end
+#template node['apache']['home'] + '/' + node['apache']['conffile'] do
+#	owner 'root'
+#	group 'root'
+#	mode '0644'
+#	source 'apache.conf.erb'
+#	action :create
+#	variables({
+#		:serverroot => node['apache']['home']
+#	})
+#	notifies :restart, "service[apache2]"
+#end
 
-cookbook_file node['apache']['home'] + '/' + node['apache']['confdir'] + '/000-main.conf' do
+template node['apache']['home'] + '/' + node['apache']['confdir'] + '/000-main.conf' do
 	owner 'root'
 	group 'root'
 	mode '0644'
-	source '000-main.conf'
+	source '000-main.conf.erb'
 	action :create
 	notifies :restart, "service[apache2]"
 end

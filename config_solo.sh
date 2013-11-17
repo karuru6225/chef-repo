7e1e6a29cf8db0ac036b328333c2afef6575eea0
kano <kano@ncdc.co.jp>
2013-11-17 22:59:58 +0900

    site-cookbooksのみでOK

--- config_solo.sh
+++ config_solo.sh
@@ -11,7 +11,7 @@ OUTPUT_KNIFE=${BASEDIR}/knife.rb
 
 cat<<EOF>$OUTPUT_SOLO
 file_cache_path "/tmp/chef-solo"
-cookbook_path ["cookbooks", "site-cookbooks"]
+cookbook_path "site-cookbooks"
 data_bag_path "data_bags"
 role_path "roles"
 EOF

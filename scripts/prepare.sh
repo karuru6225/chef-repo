7f986ed5e35b3e5ffd36169141a6cad023a42d95
kano <kano@ncdc.co.jp>
2013-11-18 08:54:11 +0900

    カレントパスが・・

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ../
-bundle install --path vendor/bundle
+bundle install --path ./vendor/bundle
 rbenv rehash
-berks install --path ../cookbooks
+berks install --path ./cookbooks
 
 ./config_solo.sh

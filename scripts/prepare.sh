408f357fec46fc3c0fe2e109814fb3051bc12c25
kano <kano@ncdc.co.jp>
2013-11-19 03:11:21 +0900

    エラーが起こる

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,9 +3,8 @@
 cd `dirname $0`; cd ../
 
 curl -L https://www.opscode.com/chef/install.sh | bash
+BASE="/opt/chef/embedded/bin"
+PATH=${BASE}:${PATH}
 bundle install
-rbenv rehash
-
 berks install --path ./cookbooks
-
 ./config_solo.sh

75e12b643954532c7c77b2263fa86c605a8882c6
加納聖大 <kano@ncdc.co.jp>
2014-01-08 07:31:49 +0900

    data_bagの暗号化を諦めた時に消し忘れていた模様

--- config_solo.sh
+++ config_solo.sh
@@ -9,17 +9,11 @@ OUTPUT_KNIFE=${BASEDIR}/knife.rb
 
 [ -d /tmp/chef-solo ] || mkdir -p /tmp/chef-solo
 
-echo -n 'Please input password'
-read PW
-
-echo ${PW} > "${BASEDIR}/key"
-
 cat<<EOF>$OUTPUT_SOLO
 file_cache_path "/tmp/chef-solo"
 cookbook_path "${BASEDIR}/cookbooks"
 data_bag_path "${BASEDIR}/data_bags"
 role_path "${BASEDIR}/roles"
-encrypted_data_bag_secret "${BASEDIR}/key"
 EOF
 
 cp $OUTPUT_SOLO $OUTPUT_KNIFE
